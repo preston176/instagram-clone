e1a457aea555b8914b62b1a3aa2aa651032aa357
preston176 <prestonnyamweya@gmail.com>
2023-06-03 23:36:30 +0300

    components added

---
+++ src/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div className='homepage'>
+    <div className='homepage__nav'>
+
+    </div>
+    <div className='homepage__timeline'>
+        
+    </div>
+    </div>
+  )
+}
+
+export default Homepage
---
+++ src/navigation/Sidenav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Sidenav.css"
+
+function Sidenav() {
+  return (
+    <div>Sidenav</div>
+  )
+}
+
+export default Sidenav
