3c0f4399a59005efbcf68a0f2cdc77acfbee86c0
preston176 <prestonnyamweya@gmail.com>
2023-06-04 01:00:15 +0300

    post component added

--- src/timeline/Timeline.js
+++ src/timeline/Timeline.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import "./Timeline.css"
 import Suggestions from './Suggestions'
+import Post from './posts/Post'
 
 function Timeline() {
   return (
     <div className='timeline'>
-    <div className='timeline__left'>Timeline</div>
+    <div className='timeline__left'>
+      <div className='timeline__posts'>
+    <Post />
+
+      </div>
+    </div>
     <div className='timeline__right'>
         <Suggestions />
     </div>
---
+++ src/timeline/posts/Post.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Post.css"
+
+function Post() {
+  return (
+    <div className='post'>This is a post</div>
+  )
+}
+
+export default Post
