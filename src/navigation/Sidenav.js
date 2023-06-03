bbf9a0161d3ff888d1fc65c1b992cd9c64a3fc3d
preston176 <prestonnyamweya@gmail.com>
2023-06-04 00:16:00 +0300

    added logo

--- src/navigation/Sidenav.js
+++ src/navigation/Sidenav.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import "./Sidenav.css"
+import HomeIcon from '@mui/icons-material/Home';
 
 function Sidenav() {
   return (
-    <div>Sidenav</div>
+    <div className='sidenav'>
+    <img className='sidenav__logo' src="https://www.pngkey.com/png/detail/1-14166_png-icon-px-instagram-line-icon-png.png alt='instagram-logo'"></img>
+
+<div className='sidenav__buttons'>
+    <div className='side__button'>
+        {/*Icons */}
+        <span><HomeIcon /></span>
+    </div>
+</div>
+    </div>
   )
 }
 
