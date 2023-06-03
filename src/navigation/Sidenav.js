4bb1cec6df0fb2ee7c4cdb64ba79a3282bed7ed3
preston176 <prestonnyamweya@gmail.com>
2023-06-04 00:44:41 +0300

    icons added to sidenav

--- src/navigation/Sidenav.js
+++ src/navigation/Sidenav.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import "./Sidenav.css"
 import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import ExploreIcon from '@mui/icons-material/Explore';
+import SlideshowIcon from '@mui/icons-material/Slideshow';
+import ChatIcon from '@mui/icons-material/Chat';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Sidenav() {
   return (
@@ -9,8 +16,33 @@ function Sidenav() {
 
 <div className='sidenav__buttons'>
     <div className='side__button'>
-        {/*Icons */}
-        <span><HomeIcon /></span>
+    <HomeIcon />
+        <span>Home</span>
+    </div>
+    <div className='side__button'>
+    <SearchIcon />
+        <span>Search</span>
+    </div>
+    <div className='side__button'>
+    <ExploreIcon />
+        <span>Explore</span>
+    </div>
+    <div className='side__button'>
+    <SlideshowIcon />
+        <span>Reels</span>
+    </div>
+    <div className='side__button'>
+    <ChatIcon />
+        <span>Messages</span>
+    </div>
+
+    <div className='side__button'>
+    <FavoriteBorderIcon />
+        <span>Notifications</span>
+    </div>
+    <div className='side__button'>
+    <AddCircleOutlineIcon />
+        <span>Create</span>
     </div>
 </div>
     </div>
