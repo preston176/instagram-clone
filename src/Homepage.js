601020ade142790dabb3c4635223006520bba6af
preston176 <prestonnyamweya@gmail.com>
2023-06-03 23:37:44 +0300

    imported to homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import "./Homepage.css"
+import Sidenav from './navigation/Sidenav'
+import Timeline from './timeline/Timeline'
 
 function Homepage() {
   return (
     <div className='homepage'>
     <div className='homepage__nav'>
-
+    <Sidenav />
     </div>
     <div className='homepage__timeline'>
-        
+    <Timeline />
     </div>
     </div>
   )
